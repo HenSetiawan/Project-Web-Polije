65e7bc545030d5e74a187ac484a11e8efb665d2a
henSetiawan <hendystyawan74@gmail.com>
2020-11-26 18:12:39 +0700

    tugas crud

--- MVC-Template/Final-Project-Template/app/views/dashboard/blog.php
+++ MVC-Template/Final-Project-Template/app/views/dashboard/blog.php
@@ -73,9 +73,9 @@
         <div class="container-fluid">
 
           <!-- Page Heading -->
-          <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+          <h1 class="h3 mb-4 text-gray-800">Daftar Blog</h1>
 
-          
+
 
         </div>
         <!-- /.container-fluid -->
