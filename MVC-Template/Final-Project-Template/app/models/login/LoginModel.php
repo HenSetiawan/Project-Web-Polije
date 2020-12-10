92b56afb74f5e5493093e2f288a5b5553af4f74c
dwirifki10 <dwirifkinovianto10@gmail.com>
2020-12-10 23:36:38 +0700

    Mengatasi permasalahan pada SMTP

--- MVC-Template/Final-Project-Template/app/models/login/LoginModel.php
+++ MVC-Template/Final-Project-Template/app/models/login/LoginModel.php
@@ -35,15 +35,12 @@ class LoginModel {
         $password = password_hash($password, PASSWORD_DEFAULT);
 
 
-<<<<<<< HEAD
-=======
+
         $insertQuery = "INSERT INTO user
                         VALUES 
                         ('$idUser','$name','$email','$password','$noHandphone','$vkey', $token)";
 
        
-
->>>>>>> ad3b935045a7080aa93342f25c0edbd4c0f5dced
         // Send an email to user
 
 		require_once './app/phpmailer/PHPMailerAutoload.php';
@@ -52,7 +49,7 @@ class LoginModel {
 		//$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
 		$mail->isSMTP();                                      // Set mailer to use SMTP
-		$mail->Host = 'smtp.hostinger.com';					      // Specify main and backup SMTP servers
+		$mail->Host = 'smtp.gmail.com';					      // Specify main and backup SMTP servers
 		$mail->SMTPAuth = true;                               // Enable SMTP authentication
 		$mail->Username = 'remanagedata@gmail.com';           // SMTP username
 		$mail->Password =  $this->auth;                       // SMTP password
@@ -82,7 +79,6 @@ class LoginModel {
 		</p>";
 
 		if ($mail->send()) {
-<<<<<<< HEAD
 			$insertQuery = "INSERT INTO user
 							VALUES 
 							('$idUser','$name','$email','$password',
@@ -99,12 +95,6 @@ class LoginModel {
 				alert('Konfirmasi email gagal dikirim');
 			</script>";
 			echo "<center><p>$mail->ErrorInfo</p><center>";
-=======
-			// insert data ke table user
-			$this->db->query($insertQuery);
-			return 1;
-		}else{
->>>>>>> ad3b935045a7080aa93342f25c0edbd4c0f5dced
 			return 0;
 		}
    	}
