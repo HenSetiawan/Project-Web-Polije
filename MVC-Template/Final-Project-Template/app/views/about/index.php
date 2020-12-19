371a4785dbb5dc520c240d300041ef3c98e0a4c5
dwirifki10 <dwirifkinovianto10@gmail.com>
2020-12-19 10:25:28 +0700

    about

--- MVC-Template/Final-Project-Template/app/views/about/index.php
+++ MVC-Template/Final-Project-Template/app/views/about/index.php
@@ -1,23 +1,24 @@
 <div class="row main-header">
-    <div class="row">
-    <h1>Koskosang</h1>
-    
-    <p>koskosang adalah jasa wibsite yang menawarkan sebuah kamar kost yang berada di wilayah daerah khusus daerah Bondowoso dengan sejumlah 
-pembayaran tertentu,Koskosang menyedian informasi kamar kosan, lengkap dengan harga kost, fasilitas kos, dan 
-dekorasi kamar beserta foto desain kamar yang sebisa mungkin mirip dengan aslinya. Situs kami telah 
-menyedian berbagai info kosan yang berada di daerah Bondowoso dengan harga murah ataupun beberapa tipe kosan lain sesuai dengan kenyaman kalian masing masing.
-</p>
-<p>Kemudahan-kemudahan yang diberikan situs kami adalah dengan adanya fitur fitur harga kost yang bisa disesuaikan dengan kebutuhan kalian, 
-tentunya fitur ini akan sangat membantu mendapatkan kost yang sesuai budget dengan lebih cepet.Koskosan juga dapat membedakan antara kosan putra dan putri
-yang berada di daerah Bondowoso sehinnga kalian tidak perlu khawatir dengan kosan yang kalian pilih, Jika kalian ingin memasan kostsan tersebut 
-maka kalian bisa langsung menghubungi si pemilik kost.
-</P>
-<p>Informasi kos yang disewakan juga langsung diberikan oleh pemilik yang pastinya sudah terkurasi kebenarannya.Pemilik kost jugak bisa menambahkan sendiri
-informasi kostnya yang akan di sewakan atau dipromosikan.
-Kostsan ini sangat berfungi sekali untuk kalian yang ingin mencari kosan yang berada di daerah khusus Bondowoso.Dengan adanya koskosang mencari kostsan
-di daerah sekitar Bondowoso akan lebih mudah dan lebih cepat.Kami selalu berusaha denngan baik untuk dapat memberikan informasi tentang kostsan di 
-daerah Bondowoso.
-</P>
+	<div class="row">
+		<div class="col-md-4">
+			<div class="row">
+				<div class="col-md-6 login-page" style="box-shadow: none !important; ">
+					<img src="<?=BASEURL;?>/public/image/koskosang.png" class="img-fluid mt-2">
+				</div>
+			</div>
+		</div>
+		<p class="mt-3 col-md-12">
+			<b>koskosang</b> adalah jasa wibsite yang menawarkan sebuah kamar kost yang berada di wilayah daerah khusus daerah Bondowoso dengan sejumlah pembayaran tertentu, <b>koskosang</b> menyedian informasi kamar kosan, lengkap dengan harga kost, fasilitas kos dan dekorasi kamar beserta foto desain kamar yang sebisa mungkin mirip dengan aslinya. Situs kami telah menyedian berbagai info kosan yang berada di daerah Bondowoso dengan harga murah ataupun beberapa tipe kosan lain sesuai dengan kenyaman kalian masing masing.
+		</p>
+
+		<p class="col-md-12">
+			Kemudahan-kemudahan yang diberikan situs kami adalah dengan adanya fitur fitur harga kost yang bisa disesuaikan dengan kebutuhan kalian, tentunya fitur ini akan sangat membantu mendapatkan kost yang sesuai budget dengan lebih cepet. <b> koskosang </b> juga dapat membedakan antara kosan putra dan putri yang berada di daerah Bondowoso sehingga kalian tidak perlu khawatir dengan kosan yang kalian pilih. Jika kalian ingin memesan kost-an tersebut maka kalian bisa langsung menghubungi si pemilik kost.
+		</p>
+
+		<p class="col-md-12">
+			Informasi kos yang disewakan juga langsung diberikan oleh pemilik yang pastinya sudah terkurasi kebenarannya. Pemilik kost jugak bisa menambahkan sendiri informasi kostnya yang akan di sewakan atau dipromosikan. Kost-an ini sangat berfungi sekali untuk kalian yang ingin mencari kosan yang berada di daerah khusus Bondowoso. Dengan adanya <b>koskosang</b> mencari kost-an di daerah sekitar Bondowoso akan lebih mudah dan lebih cepat. Kami selalu berusaha dengan baik untuk dapat memberikan informasi tentang kostsan di daerah Bondowoso.
+		</p>
+	</div>
 </div>
 </div>
 
