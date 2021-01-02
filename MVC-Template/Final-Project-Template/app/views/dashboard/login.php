274f919f1b131c66ae2512d316707d34e3ea8976
Anisa 01 <anisapuji667@gmail.com>
2021-01-02 13:17:42 +0700

    mengumpah tampilan admin

--- MVC-Template/Final-Project-Template/app/views/dashboard/login.php
+++ MVC-Template/Final-Project-Template/app/views/dashboard/login.php
@@ -37,7 +37,9 @@
                 <!-- Nested Row within Card Body -->
                 <div class="row">
                     
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                    <div class="col-lg-6 d-none d-lg-block">
+                        <img src="../public/image/logo2.png" style="width:100%;">
+                    </div>
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
