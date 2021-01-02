0f0df9cd09d7f66e5564ff7d2b65bdb03db62c08
Dewiagesti <71954394+Dewiagesti@users.noreply.github.com>
2021-01-02 12:38:07 +0700

    merubah tampilan mitra dan membuat logo

--- MVC-Template/Final-Project-Template/app/views/mitra/login.php
+++ MVC-Template/Final-Project-Template/app/views/mitra/login.php
@@ -46,7 +46,11 @@
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-                    <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+
+                    <div class="col-lg-6 d-none d-lg-block">
+                        <img src="../public/image/logo2.png" style="height:500px;">
+                    </div>
+
                     <div class="col-lg-6">
                         <div class="p-5">
                             <div class="text-center">
